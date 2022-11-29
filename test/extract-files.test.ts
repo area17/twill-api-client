6d6266da2899a4fd6741b3983d76bee9224095af
antonin caudron <antonin@area17.com>
2022-11-29 16:39:15 +0100

    add git hooks

--- test/extract-files.test.ts
+++ test/extract-files.test.ts
@@ -3,9 +3,10 @@ import pages from './data/pages.json'
 import { normalize } from '../src/normalize'
 import { deserialize } from '../src/deserialize'
 import { files } from '../src/extract/files'
+import {JsonApiDataResponse, Resource} from "../src";
 
 test('extract files', () => {
-  const normalized = normalize(pages)
+  const normalized = normalize(pages as unknown as JsonApiDataResponse)
   const deserialized = deserialize(normalized.result, normalized.resources)
   const fileResources = files(deserialized[0])
 
